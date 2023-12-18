4ad61876055989fc695bb2e1683d642bf8840f1c
Wen Sun <nlwens819@gmail.com>
2023-12-18 21:34:25 +0100

    Activated Exit Button of shop

--- shop/ExitButton.gd
+++ shop/ExitButton.gd
@@ -1,8 +1,8 @@
 extends Button
 
-
 # Function called when the exit button is pressed
-func _on_ExitButton_pressed():
-	get_tree().pop_scene()
-	
+#func _on_ExitButton_pressed():
 
+func _on_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+	pass # Replace with function body.
