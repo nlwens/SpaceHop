f400d86da12301a5e7c6ad5955b6e95cbce1a8cb
serena <127048788+serenall11@users.noreply.github.com>
2024-01-16 00:24:58 +0100

    attach money system to stars

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -3,10 +3,18 @@ extends CanvasLayer
 @onready var main = $Main
 @onready var settings = $Settings
 @onready var shop = $Shop
+@onready var money_display = get_node_or_null("world/MoneyDisplay")
+
+
+
+func _ready():
+	GameState.in_gameplay = false
 
 
 func _on_button_play_pressed():
+	GameState.in_gameplay = true
 	get_tree().change_scene_to_file("res://world.tscn")
+
 
 # Function to handle the settings button press
 func _on_button_settings_pressed():
@@ -24,5 +32,12 @@ func _on_button_shop_pressed():
 func _on_button_shop_back_pressed():
 	main.visible = true
 	shop.visible= false
+<<<<<<< Updated upstream
 	
 
+=======
+
+
+func _on_button_quit_pressed():
+	get_tree().quit()
+>>>>>>> Stashed changes
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,15 +5,31 @@ var jump_speed = 600
 var speed = 200
 var camera
 var isAlive = true
+<<<<<<< Updated upstream
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
 	camera = get_node(camera_path)
 	pass # Replace with function body.
+=======
+var deadzone_y = -1
+@export_node_path("CollisionShape2D") var feet
+@onready var money_display = get_node("../MoneyDisplay")
+
+# When starting the game
+func start_game():
+	GameState.in_gameplay = true
+
+>>>>>>> Stashed changes
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	var money_display = get_node_or_null("CanvasLayer/MoneyDisplay")
+	if money_display:
+		money_display.visible = GameState.in_gameplay
+	
 	if isAlive:
 		var left_key = Input.is_action_pressed("ui_left")
 		var right_key = Input.is_action_pressed("ui_right")
@@ -24,8 +40,33 @@ func _process(delta):
 			set_linear_velocity(Vector2(speed, ySpeed))
 	pass
 
+<<<<<<< Updated upstream
 func collision(body):		
 	if body.is_in_group('paddles') and get_linear_velocity().y > 0:
+=======
+func _on_deadzone_enter():
+	# Action when entering the dead zone (fallen down)
+	isAlive = false
+	BgAudio.stop()
+	$Fail.play()
+	print("Entered the dead zone.")
+	await get_tree().create_timer(2).timeout
+	_reset_to_main_menu()
+	# Add any other actions or behavior when the character enters the dead zone here
+
+func _reset_to_main_menu():
+	# Implement your logic to reset to the main menu here
+	# For example, you can load a new scene or reset the current scene
+	# Replace the following line with your actual code
+	money_display.visible = false  # Hide MoneyDisplay when returning to main menu
+	print("Resetting to main menu...")
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+	pass
+
+func collision(body):
+	if body.is_in_group('paddles') and get_linear_velocity().y > 0 and isAlive:
+		money_display.visible = true  # Show the MoneyDisplay when jumping
+>>>>>>> Stashed changes
 		$Jump.play()
 		set_linear_velocity(Vector2(0,-jump_speed))
 	pass
