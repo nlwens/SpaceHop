c80660d3728439fc30e9d95a25fc83045d940be4
Wen Sun <nlwens819@gmail.com>
2024-01-17 14:48:56 +0100

    recover from backup

--- Scripts/CanvasLayer.gd
+++ Scripts/CanvasLayer.gd
@@ -3,18 +3,10 @@ extends CanvasLayer
 @onready var main = $Main
 @onready var settings = $Settings
 @onready var shop = $Shop
-@onready var money_display = get_node_or_null("world/MoneyDisplay")
-
-
-
-func _ready():
-	GameState.in_gameplay = false
 
 
 func _on_button_play_pressed():
-	GameState.in_gameplay = true
 	get_tree().change_scene_to_file("res://world.tscn")
-
 
 # Function to handle the settings button press
 func _on_button_settings_pressed():
@@ -32,6 +24,7 @@ func _on_button_shop_pressed():
 func _on_button_shop_back_pressed():
 	main.visible = true
 	shop.visible= false
+	
 
 
 
