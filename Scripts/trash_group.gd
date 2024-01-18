df8af777ed773096830714df53844df700f181bb
saraalw3 <ssaraalwani@gmail.com>
2024-01-18 17:01:55 +0100

    added rest of space trash, fixed quality, fixed gravity

--- Scripts/trash_group.gd
+++ Scripts/trash_group.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var bottle_trash_scene = preload("res://scene/Space_trash.tscn")
 var banana_trash_scene = preload("res://scene/banana_trash.tscn")
+var can_trash_scene = preload("res://scene/can_trash.tscn")
+var bottle1_trash_scene = preload("res://scene/bottle1_trash.tscn")
+var apple_trash_scene = preload("res://scene/apple_trash.tscn")
 var width = get_viewport_rect().size.x
 
 func _ready():
@@ -15,11 +18,17 @@ func _on_TrashSpawnTimer_timeout():
 
 	var position_x = randf_range(-width , width )  # Random X position within the viewport width
 
-	var trash_type = randi() % 2  # Randomly choose between 0 and 1
+	var trash_type = randi() % 4  # Randomly choose between 0 and 1
 	var trash_instance
 
 	if trash_type == 0:
 		trash_instance = bottle_trash_scene.instantiate()
+	elif trash_type == 1:
+		trash_instance = can_trash_scene.instantiate()
+	elif trash_type == 2:
+		trash_instance = bottle1_trash_scene.instantiate()
+	elif trash_type == 3:
+		trash_instance = apple_trash_scene.instantiate()
 	else:
 		trash_instance = banana_trash_scene.instantiate()
 
