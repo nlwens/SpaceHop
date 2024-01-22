4acc5bad176b4cdf0a5f7cdfa5b71dc57c7d8e60
Wen Sun <nlwens819@gmail.com>
2024-01-22 14:54:59 +0100

    finished failed purchase panel

--- shop/PurchaseButton.gd
+++ shop/PurchaseButton.gd
@@ -1,8 +1,13 @@
 extends Button
 
+@onready var Failed = $Failed
+
 func _on_purchase1_pressed():
 	if Global.score >= 30:
 		Global.score -= 30
+	
+	else:
+		Failed.visible = true
 	pass # Replace with function body.
 
 func _on_purchase2_pressed():
