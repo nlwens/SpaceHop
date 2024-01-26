41eb1a42ced09903e61490cac78c7eb2ed5f3fe3
Wen Sun <nlwens819@gmail.com>
2024-01-26 12:14:15 +0100

    fixed trash sound effect playing

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -55,19 +55,26 @@ func collision(body):
 	if body.is_in_group('paddles') and get_linear_velocity().y > 0 and isAlive:
 		$Jump.play()
 		set_linear_velocity(Vector2(0, -jump_speed))
-	elif body.is_in_group('banana_trash') and isAlive:
-		$HitBanana.play()
-	elif body.is_in_group('bottle_trash') and isAlive:
-		$HitBottle.play()
-	elif body.is_in_group('apple_trash') and isAlive:
-		$HitApple.play()
-	elif body.is_in_group('bottle1_trash') and isAlive:
-		$HitBottle1.play()
-	elif body.is_in_group('can_trash') and isAlive:
-		$HitCan.play()
 	pass
 
 func whole_body(body):
+	
+	if body.is_in_group('banana_trash') and isAlive:
+		$HitBanana.play()
+		
+	if body.is_in_group('bottle_trash') and isAlive:
+		$HitBottle.play()
+		
+	if body.is_in_group('apple_trash') and isAlive:
+		$HitApple.play()
+		
+	if body.is_in_group('bottle1_trash') and isAlive:
+		$HitBottle1.play()
+		
+	if body.is_in_group('can_trash') and isAlive:
+		$HitCan.play()
+		
 	if body.is_in_group('deadzone'):
 		_on_deadzone_enter()
+		
 	pass # Replace with function body.
